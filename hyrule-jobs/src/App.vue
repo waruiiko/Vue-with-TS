85c16f2ae016752b7350230babbfd3403be92037
GinBe <asdfwxwx@gmail.com>
2021-08-10 11:28:37 +0800

    new

---
+++ hyrule-jobs/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">hello hyrule</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+});
+</script>
+
+<style>
+
+</style>
