61020a845a13f5c48af4e8b8bc4a409d77f5c0d7
GinBe <asdfwxwx@gmail.com>
2021-08-10 14:18:04 +0800

    Update App.vue

--- hyrule-jobs/src/App.vue
+++ hyrule-jobs/src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <div class="app">hello hyrule</div>
+  <div class="app">
+    hello hyrule
+    <h1>{{name}}-{{age}}</h1>
+    <input v-model="anotherName">
+    <input v-model="anotherAge">
+    <button @click="changeName">changeName</button>
+    <button @click="changeAge">changeAge</button>
+  </div>
+  
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-  }
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "xiaoming",
+      age: 30 as string | number,
+      anotherName:'',
+      anotherAge:''
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = this.anotherName;
+      return
+    },
+    changeAge() {
+      this.age = this.anotherAge;
+      return
+    },
+  },
 });
 </script>
 
 <style>
-
 </style>
